0b4367d2755d2fd0b9b94aa405491f11337e578a
105ethinday <120046286+105ethinday@users.noreply.github.com>
2022-12-21 12:15:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ const student = 'Гаврин Кирилл Андреевич';
 document.getElementById('student').innerHTML = student;
 
 
-let TEAM1A = prompt ('Введите реальный счет первой команды')
+let team1R = prompt ('Введите реальный счет первой команды')
 
-let TEAM2A = prompt ('Введите реальный счет второй команды')
+let team2R = prompt ('Введите реальный счет второй команды')
 
-let TEAM1B = prompt ('Введите предпологаемый счет первой команды')
+let team1P = prompt ('Введите предпологаемый счет первой команды')
 
-let TEAM2B = prompt ('Введите предпологаемый счет второй команды')
+let team2P = prompt ('Введите предпологаемый счет второй команды')
 
 
-if ((TEAM1A==TEAM1B) && (TEAM2A==TEAM2B))
-{alert ('Счет был угадан верно');}
+if ((team1R==team1P) && (team2R==team2P)) {
+    alert ('Счет был угадан верно');
+} else if (
+            (team1R>team2R) && (team2P>team2P)
+            ||
+            (team1R==team2R) && (team1P==team2P)
+            ||
+            (team1R<team2R) && (team1P<team2P)
+        ) {
+    alert ('Счет не угадали, но исход угадали');
 
-else if ((TEAM1A>TEAM2A) && (TEAM1B>TEAM2B))
-{alert ('Счет не угадали, но исход угадали');}
-
-else if ((TEAM1A==TEAM2A) && (TEAM1B==TEAM2B))
-{alert ('Счет не угадали, но исход угадали');}
-
-else if ((TEAM1A<TEAM2A) && (TEAM1B<TEAM2B))
-{alert ('Счет не угадали, но исход угадали');}
-
-else 
-{alert ('Ни счета, ни исхода не угадали');}
+} else {
+    alert ('Ни счета, ни исхода не угадали');
+}
